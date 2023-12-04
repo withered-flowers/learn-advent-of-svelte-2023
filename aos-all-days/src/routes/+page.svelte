723fc1aef134c714e11d6e07e37f7e93ca199097
Withered Flowers <withered-flowers@users.noreply.github.com>
2023-12-04 21:58:00 +0700

    feat: merge all to one folder, using svelte 5 beta

---
+++ aos-all-days/src/routes/+page.svelte
@@ -0,0 +1,28 @@
+<svelte:head>
+	<title>Advent of Svelte - withered-flowers</title>
+</svelte:head>
+
+<section
+	class="flex min-h-[100dvh] w-full flex-row items-center justify-center bg-gray-50 text-slate-700"
+>
+	<section class="mx-auto max-w-3xl">
+		<h1 class="text-center text-3xl font-bold text-gray-900">Advent of Svelte</h1>
+
+		<section class="grid grid-cols-7 rounded-md p-4 md:grid-cols-7">
+			<!-- Loop from 1 to 30 -->
+			{#each Array.from({ length: 24 }, (_, i) => i + 1) as day}
+				<a
+					href={`/day-${day.toString().padStart(2, '0')}`}
+					class="m-2 flex flex-col items-center justify-center rounded-md bg-white p-4 shadow-md hover:shadow-lg"
+				>
+					<span class="text-sm font-medium text-gray-500">Day</span>
+					<span class="text-2xl font-bold text-gray-900">{day.toString().padStart(2, '0')}</span>
+				</a>
+			{/each}
+		</section>
+	</section>
+</section>
+
+<style scoped>
+	@import '../app.css';
+</style>
